c6f4c2e452d7b2d30994495a8eb37f5943a9d315
Silas Correia <silas8_silva@hotmail.com>
2025-12-10 10:02:43 -0300

    refactor: templetes/add

--- secretaria/secprova/secaluno/urls.py
+++ secretaria/secprova/secaluno/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('secaluno/listar/', views.listar, name='listar'),
+    path('secaluno/add/', views.addAluno, name='add'),
     path('secaluno/', views.alunos, name='secaluno'),
 ]
